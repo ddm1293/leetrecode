3b35a7ca9ce1b03b6d7a613234fc9f7eb9d7c93f
dingmandeng <makemake1293@gmail.com>
2024-06-04 17:52:06 -0700

    add lambda factory to apiStack

--- backend/lib/stacks/api-stack.ts
+++ backend/lib/stacks/api-stack.ts
@@ -1,14 +1,29 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { LambdaFactory } from '../factories/lambda-factory';
+import { UserLambdaFactory } from '../factories/user-lambda-factory';
 
 interface ApiStackProps extends StackProps {
   table: ITable;
 }
 
 export class ApiStack extends Stack {
-  constructor(scope: Construct, id: string, props?: ApiStackProps) {
+  constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
+    // TODO: understand RestApi construct
+    const api = new RestApi(this, 'MyApi', {
+      restApiName: 'Leetrecode api gateway',
+      description: 'This service serves my API.',
+    });
+
+    this.configureFactory(new UserLambdaFactory(), api, props.table);
+  }
+
+  private configureFactory(factory: LambdaFactory, api: RestApi, table: ITable) {
+    const lambdas = factory.createLambdas(this, table);
+    factory.addRoutes(api, lambdas);
   }
 }
