d9d11792995828a5e4ca9c5c6ea4a53b0372fa2a
colinzuo <colinzuo@student.ubc.ca>
2024-07-09 08:27:56 -0700

    FE set up

---
+++ frontend/src/app/components/submissions/submission-detail/submission-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './submission-detail.component.html',
+  styleUrl: './submission-detail.component.css'
+})
+export class SubmissionDetailComponent {
+
+}
